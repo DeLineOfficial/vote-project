3d0939a9888ee1c1e0e10de9e5272540a9b993e8
StolyarAM <StolyarAM@energomera.ru>
2025-10-03 11:18:22 +0300

    post + page transition + types

--- app/app.vue
+++ app/app.vue
@@ -7,3 +7,19 @@ import '~/assets/styles/main.css'
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+
+<style scoped>
+.page-leave-active,
+.page-enter-active {
+  transition: all .2s;
+  opacity: 1;
+  right: 0px;
+  position: relative;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  right: -100px;
+}
+</style>
---
+++ shared/types/post.ts
@@ -0,0 +1,9 @@
+export interface PostContent {
+    logo: string,
+    author: string,
+    title: string,
+    content: string,
+    likes: number,
+    dislikes: number,
+    date: Date,
+}
